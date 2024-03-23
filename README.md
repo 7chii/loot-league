b59e2b848adb5261bc9ca4d20e0f23061362bb75
7chii <marinpacper@gmail.com>
2024-03-23 17:27:31 -0300

    C-Mariana-base serv

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # loot-league
 Trabalho em grupo produzido durante o primeiro semestre de 2024, um simulador de 'loot-boxes' dinamico baseado em 'League-of-Legends'
+............
---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 8080;
+
+//usar folder de paginas estaticas
+app.use('/static', express.static('static'));
+
+//pedido index(pagina html espolios)
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'static/index.html'));
+});
+
+//pedido stylesheet(CSS)
+app.get('/loot-league/static/styles.css',(req, res)=>{
+    res.set('content-type', 'text/css');
+    res.sendFile(path.join(__dirname, 'static/styles.css'));
+});
+
+//listen da porta 8080
+app.listen(port, ()=>{
+    console.log(`api listening on port ${port}`);
+});
