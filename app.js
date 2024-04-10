a53f2d056a193b4901b8a14ffdad8e379fbed816
7chii <marianapacper@outlook.com>
2024-04-09 21:22:28 -0300

    sql tabela skins add. (string passada pro front tem outras infos da entrada pra passar em <param> ou value sla no botao)

--- app.js
+++ app.js
@@ -6,66 +6,92 @@ const sql = require('mysql');
 
 //conexao BD
 const db = sql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"lootleague"
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "lootleague"
 });
 
 //usar folder de paginas estaticas
 app.use('/static', express.static('static'));
 //aumentar limite de json recebido/mandado(para passar base64)
-app.use(express.json({limit:'50mb'}));
+app.use(express.json({ limit: '50mb' }));
 
 //pedido index(pagina html espolios)
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'static/index.html'));
 });
 
 //pedido stylesheet(CSS)
-app.get('/loot-league/static/styles.css',(req, res)=>{
+app.get('/loot-league/static/styles.css', (req, res) => {
     res.set('content-type', 'text/css');
     res.sendFile(path.join(__dirname, 'static/styles.css'));
 });
 
 //pedidos imagens nao-sql
-app.get('/loot-league/static/imageAs/hextec.png',(req, res)=>{
+app.get('/loot-league/static/imageAs/hextec.png', (req, res) => {
     res.set('content-type', 'image/png');
     res.sendFile(path.join(__dirname, 'static/imageAs/hextec.png'));
 })
-app.get('/loot-league/static/imageAs/allin.png',(req, res)=>{
+app.get('/loot-league/static/imageAs/allin.png', (req, res) => {
     res.set('content-type', 'image/css');
     res.sendFile(path.join(__dirname, 'static/imageAs/allin.png'));
 })
-app.get('/loot-league/static/imageAs/bauIndl.png',(req, res)=>{
+app.get('/loot-league/static/imageAs/bauIndl.png', (req, res) => {
     res.set('content-type', 'image/png');
     res.sendFile(path.join(__dirname, 'static/imageAs/hexin.png'));
 })
-app.get('/loot-league/static/imageAs/campin.png', (req, res)=>{
+app.get('/loot-league/static/imageAs/campin.png', (req, res) => {
     res.set('content-type', 'image/png');
     res.sendFile(path.join(__dirname, 'static/imageAs/campin.png'));
 })
-app.get('/loot-league/static/imageAs/skinsin.png', (req, res)=>{
+app.get('/loot-league/static/imageAs/skinsin.png', (req, res) => {
     res.set('content-type', 'image/png');
     res.sendFile(path.join(__dirname, 'static/imageAs/skinsin.png'));
 })
 //post request de info de ID campeao, recebendo string em buffer->b64->jpg e servindo pro front:
-app.post('/img', (req, res)=>{
-    const {id} = req.body;
-    const {authorization} = req.headers;
-            const str = `SELECT splash FROM champ WHERE id='${id}'`;
-            db.query(str, (err, result)=>{
-                if (err) throw err;
-                const dat = Buffer.from(result[0].splash);
-                var string = dat.toString('base64');
-                var partS = string.split('jpegbase64');
-                res.send(`"${partS[1]}"`);
-                console.log(partS[0]+` ${id}`);
-                console.log("fim packt");
-            })
+app.post('/img', (req, res) => {
+    const { id } = req.body;
+    const { authorization } = req.headers;
+    const str = `SELECT * FROM champ WHERE id='${id}'`;
+    db.query(str, (err, result) => {
+        const dat = Buffer.from(result[0].splash);
+        const datn = Buffer.from(result[0].nome);
+        const datcs = result[0].custo.toString();
+        const datvs = result[0].valor.toString();
+        const datc = Buffer.from(datcs);
+        const datv = Buffer.from(datvs);
+        var string = dat.toString('base64');
+        var partS = string.split('jpegbase64');
+        res.send(`"${partS[1]}-${datn}-${datc}-${datv}"`);
+        /*if (err) throw err;
+        const dat = Buffer.from(result[0].splash);
+        var string = dat.toString('base64');
+        var partS = string.split('jpegbase64');
+        res.send(`"${partS[1]}"`);
+        console.log(partS[0]+` ${id}`);
+        console.log("fim packt");*/
+    })
+});
+app.post('/imgs', (req, res) => {
+    const { id } = req.body;
+    const { authorization } = req.headers;
+    const sqls = `SELECT * FROM skin WHERE id='${id}';`;
+    db.query(sqls, (err, result) => {
+        if (err) throw err;
+        const dat = Buffer.from(result[0].splash);
+        const datn = Buffer.from(result[0].nome);
+        const datcs = result[0].custo.toString();
+        const datvs = result[0].valor.toString();
+        const datc = Buffer.from(datcs);
+        const datv = Buffer.from(datvs);
+        var string = dat.toString('base64');
+        var partS = string.split('jpegbase64');
+        res.send(`"${partS[1]}-${datn}-${datc}-${datv}"`);
+    });
 });
 
 //listen da porta 8080
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`api listening on port ${port}`);
 });
